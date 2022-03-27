6005a86cd6ef9146548397866b1d64f12572a9bb
Brayden Jo <brayden.jo@outlook.com>
2022-03-27 18:24:04 +0900

    thread로 변경

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from audioop import avg
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import * 
 import pyupbit 
 import time
+import datetime
 
 f = open("./upbit.key")
 lines = f.readlines()
@@ -15,21 +15,52 @@ secret = lines[1].strip()
 upbit = pyupbit.Upbit(access, secret)
 
 
+class Worker(QThread):
+    timeout = pyqtSignal(tuple)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        while True:
+            balances = upbit.get_balances()
+
+            krw_balance = None 
+            btc_balance = None 
+            for balance in balances:
+                if balance['currency'] == "KRW" and balance['unit_currency'] == "KRW":
+                    krw_balance= balance
+                if balance['currency'] == "BTC" and balance['unit_currency'] == "KRW":
+                    btc_balance= balance
+
+            btc_price = pyupbit.get_current_price("KRW-BTC")        
+            self.timeout.emit((btc_price, krw_balance, btc_balance))
+            self.sleep(1)
+
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # variables
+        self.btc_cur_price = 0 
+        self.krw_balance_data = None
+        self.btc_balance_data = None
+
         self.unit_seed = 0
         self.unit_max = 200
         self.unit_num = 200
         self.profit_ratio = 10
         self.order_data = 0 
         self.btc_avg_buy_price = 0
-        self.initialize_unit_seed()
 
-        self.setGeometry(100, 100, 600, 300)
-        self.setWindowTitle("Cost Averaging Upbit (Bitcoin) v1.0")
+        # thread 
+        self.worker = Worker()
+        self.worker.timeout.connect(self.update_data)
+        self.worker.start()
+       
+        self.setGeometry(100, 100, 650, 400)
+        self.setWindowTitle("업비트 물타기봇 v1.0")
         self.create_table_widget()
 
         widget = QWidget()
@@ -78,7 +109,12 @@ class MyWindow(QMainWindow):
         layout_hbox = QHBoxLayout()
         self.btn_start = QPushButton("무한매수 시작")
         self.btn_start.clicked.connect(self.start)
+
+        #self.btn_sell = QPushButton("일괄 매도")
+        #self.btn_sell.clicked.connect(self.sell)
+
         layout_hbox.addWidget(self.btn_start)
+        #layout_hbox.addWidget(self.btn_sell)
         layout_hbox.addStretch(2)
 
         self.plain_text = QPlainTextEdit()
@@ -91,40 +127,61 @@ class MyWindow(QMainWindow):
 
         # timer 
         self.timer = QTimer()
-        self.timer.start(2000)
+        self.timer.start(1000)
         self.timer.timeout.connect(self.update_price)
 
         self.timer_order = QTimer()
         self.timer_order.start(1000)
         self.timer_order.timeout.connect(self.trigger_order)
 
+    @pyqtSlot(tuple)
+    def update_data(self, data):
+        self.btc_cur_price    = data[0]     
+        self.krw_balance_data = data[1]     
+        self.btc_balance_data = data[2]     
+        self.initialize_unit_seed()
+ 
     def start(self):
+        """무한매수 시작 버튼에 대한 slot
+        """
         self.order()
 
     def trigger_order(self):
         """매 초마다 호출되면서 매수 시간이 되면 order 메소드 호출
         """
+        now = datetime.datetime.now()
+        self.statusBar().showMessage(str(now)[:19])
+
         order_time = self.timeedit.time() 
         current_time = QTime.currentTime()
         diff = current_time.secsTo(order_time)
 
         if 0 <= diff < 4:
+            # 지정가 매도 주문 취소
             self.cancel_order()
+
+            # 비트코인 잔고가 0이면 거래일 익절을 의미
+            # 새로 파라미터 초기화 
+            btc_balance = upbit.get_balance("KRW-BTC")
+            if btc_balance == 0:
+                self.initialize_unit_seed()
+
             time.sleep(2)
             self.order()
             time.sleep(2)
 
     def order(self):
-        btc_price = pyupbit.get_current_price("KRW-BTC")        
-
         # 평단가가 현재가보다 낮으면 매수
-        if self.btc_avg_buy_price < btc_price:
+        if self.btc_avg_buy_price < self.btc_cur_price:
             self.order_data = upbit.buy_market_order("KRW-BTC", self.unit_seed)
             self.unit_num -= 1
 
         # 매수 후 평단 재계산 
         # 지정가 매도 
-        self.btc_avg_buy_price, volume = self.get_avg_buy_price("KRW-BTC") 
+        balance_dict = upbit.get_balance("KRW-BTC", verbose=True)
+        self.btc_avg_buy_price = float(balance_dict['avg_buy_price'])
+        volume = balance_dict['balance']
+
         price = self.btc_avg_buy_price * (1 + self.profit_ratio * 0.01)
         price = int(price)
         upbit.sell_limit_order("KRW-BTC", price, volume)
@@ -137,50 +194,56 @@ class MyWindow(QMainWindow):
             pass
 
     def initialize_unit_seed(self):
-        krw = upbit.get_balance_t("KRW")
+        krw = float(self.krw_balance_data['balance'])
         self.unit_seed = krw / self.unit_max
-
-    def get_avg_buy_price(self, ticker: str):
-        """_summary_
-
-        Args:
-            ticker (str): "KRW-BTC" 
-        """
-        avg_buy_price = 0
-        volume = 0
-        fiat, coin = ticker.split("-")
-        balances = upbit.get_balances()
-        for balance in balances:
-            if balance['currency'] == coin and balance['unit_currency'] == fiat:
-                avg_buy_price = int(balance['avg_buy_price']) 
-                volume = float(balance['balance'])
-                break
-
-        return avg_buy_price, volume
+        self.unit_num = self.unit_max
 
     def update_price(self):
-        btc_price = pyupbit.get_current_price("KRW-BTC")        
-        self.btc_avg_buy_price, volume = self.get_avg_buy_price("KRW-BTC") 
+        # 비트코인 매수 평규가 
+        try:
+            self.btc_avg_buy_price = float(self.btc_balance_data['avg_buy_price'])
+        except:
+            self.btc_avg_buy_price = 0 
 
-        item = QTableWidgetItem(format(btc_price, ","))
+        # 현재가
+        item = QTableWidgetItem(format(int(self.btc_cur_price), ","))
         item.setTextAlignment(int(Qt.AlignRight|Qt.AlignVCenter))
         self.table_widget.setItem(0, 1, item) 
 
-        item = QTableWidgetItem(format(self.btc_avg_buy_price, ","))
+        # 매수평균가
+        item = QTableWidgetItem(format(int(self.btc_avg_buy_price), ","))
         item.setTextAlignment(int(Qt.AlignRight|Qt.AlignVCenter))
         self.table_widget.setItem(0, 2, item) 
 
-        # 평가손익 
-        percent = (btc_price - self.btc_avg_buy_price) / btc_price * 100
-        item = QTableWidgetItem(f"{percent:.2f} %")
+        # 보유수량 
+        try:
+            amount = float(self.btc_balance_data['balance']) + float(self.btc_balance_data['locked'])
+        except:
+            amount = 0
+        item = QTableWidgetItem(f"{amount:.8f}")
         item.setTextAlignment(int(Qt.AlignRight|Qt.AlignVCenter))
         self.table_widget.setItem(0, 3, item) 
 
+        # 평가금액 
+        est_price = amount * self.btc_cur_price 
+        item = QTableWidgetItem(format(int(est_price), ","))
+        item.setTextAlignment(int(Qt.AlignRight|Qt.AlignVCenter))
+        self.table_widget.setItem(0, 4, item) 
+
+        # 평가손익 
+        if self.btc_avg_buy_price == 0:
+            percent = 0
+        else:
+            percent = (self.btc_cur_price - self.btc_avg_buy_price) / self.btc_cur_price * 100
+        item = QTableWidgetItem(f"{percent:.2f} %")
+        item.setTextAlignment(int(Qt.AlignRight|Qt.AlignVCenter))
+        self.table_widget.setItem(0, 5, item) 
+
         # 총보유자산 
         # 원화 및 BTC만 고려함. 
-        krw = upbit.get_balance_t("KRW")
-        btc = upbit.get_balance_t("BTC") 
-        total = krw + btc_price * btc 
+        krw = float(self.krw_balance_data['balance']) + float(self.krw_balance_data['locked'])
+        btc = float(self.btc_balance_data['balance']) + float(self.btc_balance_data['locked'])
+        total = krw + self.btc_cur_price * btc 
         total = int(total)
         self.lineedit_balance.setText(format(total, ","))
 
@@ -196,7 +259,7 @@ class MyWindow(QMainWindow):
     def create_table_widget(self):
         # table widget
         self.table_widget = QTableWidget()
-        labels = ['보유코인', '현재가', '매수평균가', '평가손익']
+        labels = ['보유코인', '현재가', '매수평균가', '보유수량', '평가금액', '평가손익']
         self.table_widget.setColumnCount(len(labels))
         self.table_widget.setRowCount(1)
         self.table_widget.verticalHeader().setVisible(False)
